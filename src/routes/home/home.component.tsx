049b102b1b7564b69ef8570e2fde9abbb891a466
georg-popp <georg.popp@outlook.com>
2023-10-17 19:14:36 +0200

    Setup react-router

---
+++ src/routes/home/home.component.tsx
@@ -0,0 +1,7 @@
+import CategoriesContainer from '../../components/categories-container/categories-container.component';
+
+const Home = () => {
+    return <CategoriesContainer />;
+};
+
+export default Home;
