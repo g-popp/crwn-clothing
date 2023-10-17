3a69a393896401a3db84ddfb9a7394fd16718b34
georg-popp <georg.popp@outlook.com>
2023-10-17 22:25:27 +0200

    setup sign-in with Google Acc

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,7 +1,20 @@
+import {
+    signInWithGooglePopup,
+    logConfig
+} from '../../utils/firebaase/firebase.utils';
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const res = await signInWithGooglePopup();
+        console.log(res.user);
+    };
+
+    logConfig();
+
     return (
         <div>
             <h1>SignIn</h1>
+            <button onClick={logGoogleUser}>Sign In With Google</button>
         </div>
     );
 };
