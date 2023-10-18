b024352c033b545e60d980e72817a19977ed9453
georg-popp <georg.popp@outlook.com>
2023-10-18 22:16:20 +0200

    Use CartContext to toggle Cart-Dropdown

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -4,13 +4,17 @@ import { Link, Outlet } from 'react-router-dom';
 import CrwnLogo from '../../assets/crown.svg?react';
 
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
 import { signOutUser } from '../../utils/firebaase/firebase.utils';
 
 import './navigation.styles.scss';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <>
@@ -33,6 +37,7 @@ const Navigation = () => {
                     )}
                     <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </>
