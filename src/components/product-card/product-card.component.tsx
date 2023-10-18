8baac71e1ce0e3a6b7729a055d62b275bf412d84
georg-popp <georg.popp@outlook.com>
2023-10-18 23:22:25 +0200

    Add functionality to add products in the cart

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
 import Button from '../button/button.component';
 import './product-card.styles.scss';
+import { CartContext } from '../../contexts/cart.context';
 
 const ProductCard = product => {
     const { id, imageUrl, name, price } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
 
     return (
         <div key={id} className='product-card-container'>
@@ -11,7 +16,9 @@ const ProductCard = product => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}€</span>
             </div>
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted' onClick={addProductToCart}>
+                Add to card
+            </Button>
         </div>
     );
 };
--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -1,13 +1,37 @@
 import { createContext, useState } from 'react';
 
+const addCartItems = (cartItems, productToAdd) => {
+    const existingCartItem = cartItems.find(
+        item => item.id === productToAdd.id
+    );
+
+    if (existingCartItem) {
+        return cartItems.map(cartItem =>
+            cartItem.id === productToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
+    }
+
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
 });
 
 export const CartPovider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen };
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = productToAdd => {
+        setCartItems(addCartItems(cartItems, productToAdd));
+    };
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
