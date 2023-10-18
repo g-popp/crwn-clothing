9703a56ef4e0a1ef408a130448047f6ccbb81bcd
georg-popp <georg.popp@outlook.com>
2023-10-18 16:00:57 +0200

    create signOut user functionality

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -3,13 +3,18 @@ import { Link, Outlet } from 'react-router-dom';
 
 import CrwnLogo from '../../assets/crown.svg?react';
 
-import './navigation.styles.scss';
 import { UserContext } from '../../contexts/user.context';
+import { signOutUser } from '../../utils/firebaase/firebase.utils';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
+    const { currentUser, setCurrentUser } = useContext(UserContext);
 
-    console.log(currentUser);
+    const signOutHandler = async () => {
+        await signOutUser();
+        setCurrentUser(null);
+    };
 
     return (
         <>
@@ -21,9 +26,15 @@ const Navigation = () => {
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
-                    <Link className='nav-link' to='/auth'>
-                        SIGN IN
-                    </Link>
+                    {currentUser ? (
+                        <span className='nav-link' onClick={signOutHandler}>
+                            SIGN OUT
+                        </span>
+                    ) : (
+                        <Link className='nav-link' to='/auth'>
+                            SIGN IN
+                        </Link>
+                    )}
                 </div>
             </div>
             <Outlet />
--- src/utils/firebaase/firebase.utils.ts
+++ src/utils/firebaase/firebase.utils.ts
@@ -4,7 +4,8 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword
+    signInWithEmailAndPassword,
+    signOut
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -68,3 +69,5 @@ export const signInUserWithEmailAndPassword = async (email, password) => {
 
     return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => await signOut(auth);
