b2ed40731d103894b48ec73f2e4b6e0513745b87
georg-popp <georg.popp@outlook.com>
2023-10-19 14:28:53 +0200

    Rendering ProductCards

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -1,18 +1,25 @@
-import { useContext } from 'react';
+import { Fragment, useContext } from 'react';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { categoriesMap: products } = useContext(CategoriesContext);
+  const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <div className="products-container">
-      {products.map((product) => (
-        <ProductCard key={product.id} {...product} />
+    <>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} {...product} />
+            ))}
+          </div>
+        </Fragment>
       ))}
-    </div>
+    </>
   );
 };
 
