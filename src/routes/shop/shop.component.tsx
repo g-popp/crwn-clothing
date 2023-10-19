39418ca253c7c79577c9aeccc7604d925c2a26ca
georg-popp <georg.popp@outlook.com>
2023-10-19 18:02:17 +0200

    Create categories-preview component and routing

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -1,25 +1,14 @@
-import { Fragment, useContext } from 'react';
-import { CategoriesContext } from '../../contexts/categories.context';
-import ProductCard from '../../components/product-card/product-card.component';
+import { Routes, Route } from 'react-router-dom';
+
+import CategoriesPreview from '../categories-preview/categories-preview.component';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} {...product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 
