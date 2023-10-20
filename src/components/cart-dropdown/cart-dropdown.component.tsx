f4cc635156905f266bd8597305e9504626b5f1f4
georg-popp <georg.popp@outlook.com>
2023-10-20 15:22:38 +0200

    Refactor cart-dropdown to styled-component

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -4,8 +4,13 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
 import { CartContext } from '../../contexts/cart.context';
+
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from './cart-dropdown.styles';
 
 const CartDropdown = () => {
   const { cartItems, cartTotal } = useContext(CartContext);
@@ -16,15 +21,19 @@ const CartDropdown = () => {
   };
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} cartItem={cartItem} />
-        ))}
-      </div>
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
 
       <Button onClick={navigateToCheckout}>CHECKOUT {`(€${cartTotal})`}</Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
