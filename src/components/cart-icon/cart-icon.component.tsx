8278e0123553227388d4854a617c34a8094e4185
georg-popp <georg.popp@outlook.com>
2023-10-20 14:48:47 +0200

    Refactor cart-icon-component to styled-component

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,8 +1,8 @@
-import { useContext, useEffect, useState } from 'react';
-import ShoppingIcon from '../../assets/shopping-bag.svg?react';
+import { useContext } from 'react';
 
-import './cart-icon.styles.scss';
 import { CartContext } from '../../contexts/cart.context';
+
+import { CartIconContainer, ItemCount, ShoppingIcon } from './cart-icon.styles';
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
@@ -10,10 +10,10 @@ const CartIcon = () => {
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon />
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
